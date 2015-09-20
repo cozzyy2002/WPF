f4a063777c413eaf5b5d1727e3834cd2034d5129
cozzyy2002 <cozzyy2002@gmail.com>
2015-09-21 00:03:35 +0900

    Move HwndHost to VideoPreview project

---
+++ VideoPreview/HWndHostControl.cpp
@@ -0,0 +1,41 @@
+#include "stdafx.h"
+#include "HWndHostControl.h"
+
+using namespace System;
+using namespace System::Runtime::InteropServices;
+using namespace System::Windows::Controls;
+using namespace Win32;
+
+CHWndHostControl::CHWndHostControl()
+{
+}
+
+void CHWndHostControl::attach(Decorator^ parent)
+{
+	this->parent = parent;
+	this->parent->Child = this;
+}
+
+HandleRef CHWndHostControl::BuildWindowCore(HandleRef hwndParent)
+{
+	this->Width = this->parent->ActualWidth;
+	this->Height = this->parent->ActualHeight;
+
+	HWND hwnd = CreateWindowEx(
+					0, _T("Static"), _T(""), WS_CHILD | WS_VISIBLE, 0, 0,
+					(int)this->Width, (int)this->Height,
+					(HWND)hwndParent.Handle.ToPointer(), NULL, NULL, NULL);
+	if(!hwnd) Console::WriteLine("CreateWindowEx() failed. error=0x{0:x}", GetLastError());
+	return HandleRef(this, IntPtr(hwnd));
+}
+
+void CHWndHostControl::DestroyWindowCore(HandleRef hwnd)
+{
+	DestroyWindow((HWND)hwnd.Handle.ToPointer());
+}
+
+IntPtr CHWndHostControl::WndProc(IntPtr hwnd, int msg, IntPtr wParam, IntPtr lParam, bool% handled)
+{
+	handled = false;
+	return IntPtr(NULL);
+}
---
+++ VideoPreview/HWndHostControl.h
@@ -0,0 +1,25 @@
+#pragma once
+
+namespace Win32 {
+
+	public ref class CHWndHostControl :
+		public System::Windows::Interop::HwndHost {
+	public:
+		CHWndHostControl();
+		void attach(System::Windows::Controls::Decorator^ parent);
+
+	protected:
+		virtual System::Runtime::InteropServices::HandleRef BuildWindowCore(System::Runtime::InteropServices::HandleRef hwndParent) override;
+		virtual void DestroyWindowCore(System::Runtime::InteropServices::HandleRef hwnd) override;
+		virtual System::IntPtr WndProc(
+										System::IntPtr hwnd,
+										int msg,
+										System::IntPtr wParam,
+										System::IntPtr lParam,
+										bool% handled
+		) override;
+
+		System::Windows::Controls::Decorator^ parent;
+	};
+
+}
