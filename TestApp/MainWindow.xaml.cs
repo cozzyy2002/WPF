dc9343586042df5983b8907014225dbba99db2ba
cozzyy2002 <cozzyy2002@gmail.com>
2015-10-10 23:48:06 +0900

    Change to display native culture name in ComboBox Show MessageBox when Language selection of ComboBox changed

--- TestApp/MainWindow.xaml.cs
+++ TestApp/MainWindow.xaml.cs
@@ -27,9 +27,11 @@ namespace TestApp
                     case "CultureInfo":
                         CultureInfo value = Settings.CultureInfo;
                         Console.WriteLine("Current culture={0}", value.EnglishName);
-                        value.ClearCachedData();
-                        Thread.CurrentThread.CurrentCulture = value;
-                        Thread.CurrentThread.CurrentUICulture = value;
+                        Dispatcher.BeginInvoke((Action)(() => {
+                            Thread.CurrentThread.CurrentCulture = value;
+                            Thread.CurrentThread.CurrentUICulture = value;
+                            MessageBox.Show(DateTime.Now.ToString("F"), value.NativeName, MessageBoxButton.YesNoCancel, MessageBoxImage.Information);
+                        }));
                         break;
                 }
             };
