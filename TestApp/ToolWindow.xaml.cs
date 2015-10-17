dc281d4b2a6aeeb8498ad498ea65389361bcb2b4
cozzyy2002 <cozzyy2002@gmail.com>
2015-10-17 18:15:21 +0900

    Change so that LCID in DataGrid can be edited

--- TestApp/ToolWindow.xaml.cs
+++ TestApp/ToolWindow.xaml.cs
@@ -40,32 +40,50 @@ namespace TestApp
         {
             public CultureInfo() { }
             public CultureInfo(string name) { this.Name = name; }
+            public CultureInfo(int lcid) { this.LCID = lcid; }
 
             public string Name
             {
                 get { return (inner == null) ? "" : inner.Name; }
                 set
                 {
-                    Console.WriteLine("CultureInfo: Changing name: '{0}' -> '{1}'", (inner == null) ? "null" : Name, value);
                     if ((inner == null) || (inner.Name != value)) {
+                        Console.WriteLine("CultureInfo: Changing Name: '{0}' -> '{1}'", Name, value);
                         inner = new System.Globalization.CultureInfo(value);
-                        if (PropertyChanged != null)
-                        {
-                            foreach (string name in propertyNames)
-                            {
-                                PropertyChanged(this, new PropertyChangedEventArgs(name));
-                            }
-                        }
+                        onPropertyChanged();
                     }
                 }
             }
 
+            public int LCID
+            {
+                get { return (inner == null) ? 0 : inner.LCID; }
+                set
+                {
+                    if ((inner == null) || (inner.LCID != value))
+                    {
+                        Console.WriteLine("CultureInfo: Changing LCID: '{0}' -> '{1}'", LCID, value);
+                        inner = new System.Globalization.CultureInfo(value);
+                        onPropertyChanged();
+                    }
+                }
+            }
+
+            void onPropertyChanged()
+            {
+                if (PropertyChanged != null)
+                {
+                    foreach (string name in propertyNames)
+                    {
+                        PropertyChanged(this, new PropertyChangedEventArgs(name));
+                    }
+                }
+            }
             static readonly string[] propertyNames = { "Name", "DisplayName", "EnglishName", "NativeName", "LCID", };
 
             public string DisplayName { get { return (inner == null) ? "" : inner.DisplayName; } }
             public string EnglishName { get { return (inner == null) ? "" : inner.EnglishName; } }
             public string NativeName { get { return (inner == null) ? "" : inner.NativeName; } }
-            public int LCID { get { return (inner == null) ? 0 : inner.LCID; } }
 
             System.Globalization.CultureInfo inner = null;
 
