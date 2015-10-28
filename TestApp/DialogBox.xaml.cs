0ea89cfd5b3545b3181dc3bdf349a2e1b54be8ad
cozzyy2002 <cozzyy2002@gmail.com>
2015-10-28 23:44:35 +0900

    Change DialogBox to select contents by DialogType constructor parameter

--- TestApp/DialogBox.xaml.cs
+++ TestApp/DialogBox.xaml.cs
@@ -1,27 +1,30 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
 
 namespace TestApp
 {
+    public enum DialogType
+    {
+        Progress, Error
+    }
+
     /// <summary>
     /// Interaction logic for DialogBox.xaml
     /// </summary>
     public partial class DialogBox : Window
     {
-        public DialogBox()
+        public DialogBox(DialogType type)
         {
+            this.DialogType = type;
+
             InitializeComponent();
+        }
+
+        public DialogType DialogType { get; protected set; }
+
+        private void onTestButtonClick(object sender, RoutedEventArgs e)
+        {
+            Console.WriteLine("onTestButtonClick");
         }
     }
 }
