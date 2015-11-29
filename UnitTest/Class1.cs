5bdee4a5ad19306384e5bfe47afe820c3caee8b3
cozzyy2002 <cozzyy2002@gmail.com>
2015-11-29 20:29:11 +0900

    Add NUnit.Runners.3.0.0 as extension Remove NUnitTestAdapter.2.0.0

--- UnitTest/Class1.cs
+++ UnitTest/Class1.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using NUnit.Framework;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,12 @@ using System.Threading.Tasks;
 
 namespace UnitTest
 {
+    [TestFixture]
     public class Class1
     {
+        [Test]
+        public void test1()
+        {
+        }
     }
 }
