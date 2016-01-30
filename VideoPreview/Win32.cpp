6f969f882d813ed5589e862b002bdd4a32853cfd
cozzyy2002 <cozzyy2002@gmail.com>
2016-01-30 23:08:10 +0900

    Convert HResult to message

--- VideoPreview/Win32.cpp
+++ VideoPreview/Win32.cpp
@@ -1,13 +1,22 @@
 #include "stdafx.h"
 #include "Win32.h"
 
+#pragma comment(lib, "Quartz.lib")
+
 using namespace System;
 
 HRESULT Win32::hResultCheck(HRESULT hr, String^ method)
 {
 	if(FAILED(hr)) {
-		String^ msg = String::Format("'{1}' failed. error=0x{0:x}", hr, method);
-		throw gcnew Win32::ComOperationFailedException(msg, hr);
+		String^ hrMsg = nullptr;
+		TCHAR buffer[MAX_ERROR_TEXT_LEN];
+		DWORD ret = AMGetErrorText(hr, buffer, MAX_ERROR_TEXT_LEN);
+		if(0 < ret) {
+			hrMsg = gcnew String(buffer);
+		}
+
+		String^ msg = String::Format("'{0}' failed. {1}(0x{2:x})", method, (hrMsg != nullptr) ? hrMsg : "<unknown HRESULT>", hr);
+			throw gcnew Win32::ComOperationFailedException(msg, hr);
 	}
 	return hr;
 }
