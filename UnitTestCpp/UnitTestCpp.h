d5dbf3ce810ac3bd165b8d50338c11bf6f8826b7
cozzyy2002 <cozzyy2002@gmail.com>
2016-03-13 22:19:03 +0900

    Add UnitTestCpp project including test for HRESULT_CHECK

---
+++ UnitTestCpp/UnitTestCpp.h
@@ -0,0 +1,50 @@
+// UnitTestCpp.h
+
+#pragma once
+
+#include "VideoPreview\Win32.h"
+
+using namespace NUnit::Framework;
+using namespace System;
+using namespace Win32;
+
+namespace UnitTestCpp {
+
+	ref class Result {
+	public:
+		Result(HRESULT hr) : hr(hr) {}
+		void result() { HRESULT_CHECK(hr); }
+
+		HRESULT hr;
+	};
+
+	[TestFixture]
+	public ref class Win32Test
+	{
+	public:
+		[Test]
+		void noError() {
+			Assert::That(HRESULT_CHECK(S_OK), Is::EqualTo(S_OK));
+		}
+		[Test]
+		void noError_Except() {
+			HRESULT hr = E_ACCESSDENIED;
+			Assert::That(HRESULT_CHECK_EX(hr, hr), Is::EqualTo(hr));
+		}
+		[Test]
+		void noError_ExceptAny() {
+			HRESULT hrs[] = {E_ABORT, E_ACCESSDENIED, S_OK};
+			Assert::That(HRESULT_CHECK_EX(E_ACCESSDENIED, hrs), Is::EqualTo(E_ACCESSDENIED));
+		}
+		[Test]
+		void error()
+		{
+			Result^ result = gcnew Result(E_ABORT);
+
+			ComOperationFailedException^ ex =
+			Assert::Throws<ComOperationFailedException^>(gcnew TestDelegate(result, &Result::result));
+			Assert::That(ex->HResult, Is::EqualTo(result->hr));
+			Assert::That(ex->InnerException->HResult, Is::EqualTo(result->hr));
+		}
+	};
+}
