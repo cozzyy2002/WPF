6db4b5a65e5bf4e3f193e5ef8234d8f467df38c8
cozzyy2002 <cozzyy2002@gmail.com>
2018-10-18 22:24:26 +0900

    Add ie.WPF project.  * Implemented simple WebBrowser navigation.

---
+++ ie.WPF/MainWindow.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System.Windows;
+
+namespace ie.WPF
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void GoButton_Click(object sender, RoutedEventArgs e)
+        {
+            webBrowser.Navigate(UrlText.Text);
+        }
+    }
+}
