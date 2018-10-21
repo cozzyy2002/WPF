4ed4f0d94db0546e2d74da9be0bd6b3326fbc8e4
cozzyy2002 <cozzyy2002@gmail.com>
2018-10-21 22:14:09 +0900

    Add status bar.

--- ie.WPF/MainWindow.xaml.cs
+++ ie.WPF/MainWindow.xaml.cs
@@ -7,14 +7,41 @@ namespace ie.WPF
     /// </summary>
     public partial class MainWindow : Window
     {
+        public string TargetUrl
+        {
+            get { return (string)GetValue(TargetUrlProperty); }
+            set { SetValue(TargetUrlProperty, value); }
+        }
+
+        public static readonly DependencyProperty TargetUrlProperty
+            = DependencyProperty.Register("TargetUrl", typeof(string), typeof(MainWindow));
+
+        public string CurrentUrl
+        {
+            get { return (string)GetValue(CurrentUrlProperty); }
+            private set { SetValue(CurrentUrlProperty, value); }
+        }
+
+        public static readonly DependencyProperty CurrentUrlProperty
+            = DependencyProperty.Register("CurrentUrl", typeof(string), typeof(MainWindow));
+
         public MainWindow()
         {
             InitializeComponent();
+
+            this.TargetUrl = "https://beta.ucs.ricoh.com/util/login";
+            this.webBrowser.LoadCompleted += WebBrowser_LoadCompleted;
+        }
+
+        private void WebBrowser_LoadCompleted(object sender, System.Windows.Navigation.NavigationEventArgs e)
+        {
+            CurrentUrl = e.Uri.AbsoluteUri + ": LoadCompleted";
         }
 
         private void GoButton_Click(object sender, RoutedEventArgs e)
         {
-            webBrowser.Navigate(UrlText.Text);
+            CurrentUrl = this.TargetUrl + ": Navigating";
+            webBrowser.Navigate(this.TargetUrl);
         }
     }
 }
